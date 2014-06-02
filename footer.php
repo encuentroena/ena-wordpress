56dd300ad9f347babc04718d144dcd8d1d77ef70
encuentroena <info@encuentrodenuestraamerica.org>
2014-06-02 18:06:07 -0300

    ubicacion del footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 				<?php if ( apply_filters( 'show_flat_credits', true ) ) { ?>
 					<div class="site-info">
+						<div id="footer-image"></div>
 						<?php do_action( 'flat_credits' ); ?>
 						<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
 						<?php printf( __( 'Theme: %1$s by %2$s.', 'flat' ), 'Flat', '<a href="'.esc_url( 'http://www.yoarts.com/' ).'" title="'.esc_attr('Webmaster Tutorials & Resources').'">YoArts</a>' ); ?>

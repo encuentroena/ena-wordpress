be9580c74b3f3f7e4798ffa30b388a5ca1d878d2
encuentroena <info@encuentrodenuestraamerica.org>
2014-06-03 14:30:42 -0300

    texto del footer

--- footer.php
+++ footer.php
@@ -2,9 +2,10 @@
 					<div class="site-info">
 						<div id="footer-image" style='background: url("wp-content/themes/ena-wordpress/images/footer02.png") no-repeat scroll 0 0 rgba(0, 0, 0, 0);'></div>
 						<?php do_action( 'flat_credits' ); ?>
-						<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
-						<?php printf( __( 'Theme: %1$s by %2$s.', 'flat' ), 'Flat', '<a href="'.esc_url( 'http://www.yoarts.com/' ).'" title="'.esc_attr('Webmaster Tutorials & Resources').'">YoArts</a>' ); ?>
-					</div><!-- .site-info -->
+						<div id="footer-licence">
+							Copyleft 2013/2014 - Encuentro de Nuestra América<br/><b>¿Quieres contactarnos? <a href="http://www.encuentrodenuestraamerica.org/contacto">Envíanos un correo electrónico</a></b><br />
+													</div>
+											</div><!-- .site-info -->
 				<?php } ?>
 			</div>
 		</div>

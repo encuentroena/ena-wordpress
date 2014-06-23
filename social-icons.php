9971b77f1bcf9007e9977ceed7ef85037539f071
encuentroena <info@encuentrodenuestraamerica.org>
2014-06-23 00:25:37 -0300

    estilizacion y configuracion de socialicons

--- social-icons.php
+++ social-icons.php
@@ -1,10 +1,13 @@
-<a class="footer-btn share-btn" title="" id="footer-btn-fb"  href="#"
-onclick="return !window.open('https://www.facebook.com/sharer/sharer.php?u=<?php echo get_post_meta($post->ID, '_yourls_url', true); ?> '+'','das','location=no,links=no,scrollbars=no,toolbar=no,width=620,height=550')"
+
+
+<div id="barra-social">
+<div class="compartir">Compartir:</div>
+<a class="logo-social share-btn" title="" id="facebook"  href="#"
+onclick="return !window.open('https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink() ?> '+'','das','location=no,links=no,scrollbars=no,toolbar=no,width=620,height=550')"
 target="_blank"></a>
 
-<a class="footer-btn share-btn" title="" id="footer-btn-tw" href="#" onclick="return !window.open('http://twitter.com/home/?status=<?php echo the_title();?> <?php echo get_post_meta($post->ID, '_yourls_url', true);?> <?php echo "#Ena"?>','das','location=no,links=no,scrollbars=no,toolbar=no,width=620,height=350')"
+<a class="logo-social share-btn" title="" id="twitter" href="#" onclick="return !window.open('http://twitter.com/home/?status=<?php echo the_title();?> <?php echo get_permalink() ?> <?php echo "#Ena"?>','das','location=no,links=no,scrollbars=no,toolbar=no,width=620,height=350')"
 target="_blank"></a>
 
-<a class="footer-btn share-btn" style="display:none;" data-reveal-id="footer-btn-share-popup" title="" id="footer-btn-share" onclick="" href="#"></a>
-
-<a id="footer-btn-rss" title="" class="footer-btn share-btn"  style="display:none;" href="<?php echo get_post_meta($post->permalink); ?>feed/" target="_blank"></a>
+	
+</div>

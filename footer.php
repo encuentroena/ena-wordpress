65fcfb9522042ac161fc84d29eda9dfca9c18e17
encuentroena <info@encuentrodenuestraamerica.org>
2014-06-27 23:21:16 -0300

    fondos random en footer

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
 				<?php if ( apply_filters( 'show_flat_credits', true ) ) { ?>
-					<div class="espacio"></div>
+				<div class="espacio"></div>
 					<div class="site-info">
-						<div id="footer-image" style='background: url("/wp-content/themes/ena-wordpress/images/footer02.png") no-repeat scroll 0 0 rgba(0, 0, 0, 0);'></div>
+						<div id="footer-image" style='background: url("/wp-content/themes/ena-wordpress/images/footer<?php
+					$a=array("02","03","04","05","01");
+					echo $a[array_rand($a)];
+					?>.png") no-repeat scroll 0 0 rgba(0, 0, 0, 0);'></div>
 						<?php do_action( 'flat_credits' ); ?>
 						<div id="footer-licence">
+											
 							Copyleft 2013/2014 - Encuentro de Nuestra América<br/><b>¿Quieres contactarnos? <a href="http://www.encuentrodenuestraamerica.org/contacto">Envíanos un correo electrónico</a></b><br />
 													</div>
 											</div><!-- .site-info -->
